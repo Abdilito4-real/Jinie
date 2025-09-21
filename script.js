c082db04a8523685fdfb466e2e473756919b2215
Abdilito4-real <abdallahabubakar269@gmail.com>
2025-09-21 08:33:27 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mobile nav toggle
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+  menuToggle.classList.toggle("active");
+});
